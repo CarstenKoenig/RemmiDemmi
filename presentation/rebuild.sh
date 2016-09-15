d3532c313691724891de81e99849eb0871716ee1
Carsten König <carsten@gettingsharper.de>
2016-09-15 13:28:01 +0200

    gh-pages geleert

--- presentation/rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if test "$OS" = "Windows_NT"
-then
-  # use .Net
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
-else
-  # use mono
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
-fi
